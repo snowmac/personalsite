3f6b9a24e480831624c0b7603aed874ed409a592
Adam Bourg <Adam.bourg@gmail.com>
2017-03-17 10:31:42 -0600

    Adding easy script to add blog posts

--- newPost.sh
+++ newPost.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
+timestamp=$(date +'%Y-%m-%d')
+argumentsAsString="$*"
+hypened="${argumentsAsString// /-}"
+filename="_posts/$timestamp-$hypened.md"
 
-echo $1
+touch $filename
+
+echo "---" >> $filename
+echo "layout: post" >> $filename
+echo "title: $*" >> $filename
+echo "---" >> $filename
+
